920176c021b9c68f9284e5957485a41f7a14b150
andreyfelipe01 <andreyfelipee@yahoo.com>
2023-11-29 04:57:59 -0400

    Update BookList.js

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { FlatList } from 'react-native';
+import { FlatList , Image , StyleSheet} from 'react-native';
 import Item from './Item';
+import { ScrollView } from 'react-native-virtualized-view';
 
 const BookList = ({ books }) => {
   const renderItem = ({ item }) => (
@@ -9,17 +10,23 @@ const BookList = ({ books }) => {
       autor={item.autor}
       descricao={item.descricao}
       imagem={item.imagem}
+      
       onPress={() => console.log('Livro pressionado:', item.titulo)}
     />
+    
   );
 
   return (
+
     <FlatList
       data={books}
       renderItem={renderItem}
       keyExtractor={item => item.id}
+      style={{maxHeight:'63%'}}
     />
+
   );
 };
 
 export default BookList;
+
